a390f4b0df3a83dbc963fbe8b457a02a6e15dfc2
Jordan Garcia <jordan.garcia@infometrika.com>
2024-10-23 14:30:30 -0500

    estandares y bd

--- SGOC/BD.sql
+++ SGOC/BD.sql
@@ -24,11 +24,12 @@ CREATE TABLE `usuarios` (
   `perfil` int(11) NOT NULL,
   `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 /*Data for the table `usuarios` */
 
 insert  into `usuarios`(`id`,`nombres`,`apellidos`,`usuario`,`pass`,`perfil`,`fecha`) values (1,'Jordan','Dev','jgarcia','$2y$10$jQIj/EufiIZG03i65kImDuAvZShhlIkAHLE22sq5bhVtVW1TudG32',0,'2024-10-22 11:28:18');
+insert  into `usuarios`(`id`,`nombres`,`apellidos`,`usuario`,`pass`,`perfil`,`fecha`) values (2,'Daniel','Marquez','dmarquez',NULL,0,'2024-10-23 14:29:34');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;

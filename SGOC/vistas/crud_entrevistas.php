dcecc659481bb512e581c63b751a6c5ec43b46b3
Orlandomrqez <programacionline2@gmail.com>
2024-10-28 16:38:22 -0500

    vitas nuevas

--- SGOC/vistas/crud_entrevistas.php
+++ SGOC/vistas/crud_entrevistas.php
@@ -1,107 +1,85 @@
-<?php
-include('../app/config.php');
-<<<<<<< Updated upstream
-$titulo = "Pruebas Técnicas"; // Título específico para esta vista
-=======
-$titulo = "Entrevistas"; // Título específico para esta vista
->>>>>>> Stashed changes
-include(LAYOUT.'/header.php');
-?>
-<div class="wrapper">
-    <?php include(LAYOUT .'/sidebar.php');?>
-
-    <div class="main">
-        <?php include(LAYOUT .'/navbar.php');?>
-
-        <main class="content">
-            <div class="container-fluid p-0">
-                <div class="mb-3">
-<<<<<<< Updated upstream
-                    <h1 class="h3 d-inline align-middle"><i class="align-middle me-2 fas fa-fw fa-cogs"></i><?=$titulo;?></h1>
-=======
-                    <h1 class="h3 d-inline align-middle"><i class="align-middle me-2 fas fa-fw fa-cogs"></i>Pruebas Técnicas</h1>
->>>>>>> Stashed changes
-                    <button class="btn btn-primary float-end" id="addNuevo" data-bs-toggle="modal" data-bs-target="#modalCRUD"><i class="align-middle me-2 fas fa-fw fa-plus"></i>Agregar Nuevo</button>
-                </div>
-
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h5 class="card-title">Crea/Edita/Elimina las Entrevistas</h5>
-                            </div>
-                            <div class="card-body">
-                                <table id="datosTabla" class="table table-striped" style="width:100%">
-                                    <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Nombre del empleado</th>
-                                        <th>Fecha</th>
-                                        <th>Acciones</th>
-                                    </tr>
-                                    </thead>
-                                    <tfoot>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Nombre del empleado</th>
-                                        <th>Fecha</th>
-                                        <th>Acciones</th>
-                                    </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Modal para agregar/editar -->
-                <div class="modal fade" id="modalCRUD" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="modalLabel"><i class="align-middle me-2 far fa-fw fa-folder"></i>Nueva Entrevista</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <form id="formDatosCrud">
-                                    <input type="hidden" id="id" name="id">
-                                    <div class="mb-3">
-                                        <label for="emp_id" class="form-label">Empleados</label>
-                                        <select class="form-select" id="emp_id" name="emp_id" required="">
-                                            <option value="">Seleccione un empleado</option>
-                                        </select>
-                                    </div>
-                                    <span id="tpregunta"></span>
-                                    <span id="pregunta"></span>
-
-                                    <div class="mb-3 col-md-12">
-                                        <div id="preguntasContainer">
-                                            <!-- Las preguntas se llenarán aquí -->
-                                        </div>
-                                    </div>
-
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="align-middle me-2 fas fa-fw fa-minus"></i>Cancelar</button>
-                                        <button type="submit" class="btn btn-primary"><i class="align-middle me-2 fas fa-fw fa-save"></i>Guardar</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Fin del modal -->
-            </div>
-        </main>
+<main class="content">
+<div class="container-fluid p-0">
+    <div class="mb-3">
+        <h1 class="h3 d-inline align-middle"><i class="align-middle me-2 fas fa-fw fa-cogs"></i>Entrevistas</h1>
+        <button class="btn btn-primary float-end" id="addNuevo" data-bs-toggle="modal" data-bs-target="#modalCRUD"><i class="align-middle me-2 fas fa-fw fa-plus"></i>Agregar Nuevo</button>
     </div>
-</div>
 
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">Crea/Edita/Elimina las Entrevistas</h5>
+                </div>
+                <div class="card-body">
+                    <table id="datosTabla" class="table table-striped" style="width:100%">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre del empleado</th>
+                            <th>Fecha</th>
+                            <th>Acciones</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre del empleado</th>
+                            <th>Fecha</th>
+                            <th>Acciones</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal para agregar/editar -->
+    <div class="modal fade" id="modalCRUD" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabel"><i class="align-middle me-2 far fa-fw fa-folder"></i>Nueva Entrevista</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="formDatosCrud">
+                        <input type="hidden" id="id" name="id">
+                        <div class="mb-3">
+                            <label for="emp_id" class="form-label">Empleados</label>
+                            <select class="form-select" id="emp_id" name="emp_id" required="">
+                                <option value="">Seleccione un empleado</option>
+                            </select>
+                        </div>
+                        <span id="tpregunta"></span>
+                        <span id="pregunta"></span>
+
+                        <div class="mb-3 col-md-12">
+                            <div id="preguntasContainer">
+                                <!-- Las preguntas se llenarán aquí -->
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="align-middle me-2 fas fa-fw fa-minus"></i>Cancelar</button>
+                            <button type="submit" class="btn btn-primary"><i class="align-middle me-2 fas fa-fw fa-save"></i>Guardar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin del modal -->
+</div>
+</main>
 <script>
     $(document).ready(function() {
 
         function llenarSelects(tipoPreguntaID = '') {
             // Llenar el select de empleados
             $.ajax({
-                url: '<?= API ?>pruebas_tecnicas.php?action=empleados',
+                url: '<?= API ?>entrevistas.php?action=empleados',
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
@@ -122,7 +100,7 @@ include(LAYOUT.'/header.php');
             });
             // Llenar las preguntas y respuestas
             $.ajax({
-                url: '<?= API ?>pruebas_tecnicas.php?action=questions',
+                url: '<?= API ?>entrevistas.php?action=questions',
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
@@ -154,7 +132,7 @@ include(LAYOUT.'/header.php');
         llenarSelects();
 
         var table = $('#datosTabla').DataTable({
-            "ajax": "<?= API ?>pruebas_tecnicas.php?action=fetch",
+            "ajax": "<?= API ?>entrevistas.php?action=fetch",
             "columns": [
                 { "data": "idEntrevista" },
                 { "data": "nombre_completo" },
@@ -180,7 +158,7 @@ include(LAYOUT.'/header.php');
             var formData = $(this).serialize();
 
             $.ajax({
-                url: '<?= API ?>pruebas_tecnicas.php?action=save',
+                url: '<?= API ?>entrevistas.php?action=save',
                 type: 'POST',
                 data: formData,
                 success: function(response) {
@@ -202,7 +180,7 @@ include(LAYOUT.'/header.php');
             var id = $(this).data('id');
 
             $.ajax({
-                url: '<?= API ?>pruebas_tecnicas.php?action=edit&id=' + id,
+                url: '<?= API ?>entrevistas.php?action=edit&id=' + id,
                 type: 'GET',
                 dataType: 'json',
                 success: function(entrevista) {
@@ -223,7 +201,7 @@ include(LAYOUT.'/header.php');
                 var id = $(this).data('id');
 
                 $.ajax({
-                    url: '<?= API ?>pruebas_tecnicas.php?action=delete&id=' + id,
+                    url: '<?= API ?>entrevistas.php?action=delete&id=' + id,
                     type: 'GET',
                     success: function(response) {
                         table.ajax.reload();
@@ -233,7 +211,3 @@ include(LAYOUT.'/header.php');
         });
     });
 </script>
-
-<?php
-include(LAYOUT.'/footer.php');
-?>

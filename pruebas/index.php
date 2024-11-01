dc6708679092e34a2d4883215bf983d70ce7e5cd
Jordan Garcia <jordan.garcia@infometrika.com>
2024-11-01 10:26:48 -0500

    Update index.php

--- pruebas/index.php
+++ pruebas/index.php
@@ -8,7 +8,7 @@ include('app/config.php');
 // Verifica si el usuario está logueado
 if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
     // Redirige al login si no está logueado
-    header("Location: " . VIEW . "/vistas/login.php");
+    header("Location: " . VIEW . "/login.php");
     exit();
 } else {
     // Si está logueado, redirige al dashboard
